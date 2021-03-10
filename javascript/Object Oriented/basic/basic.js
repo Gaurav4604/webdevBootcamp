55d3a570a0432e4217d459a3a89eb80681095e0d
Gaurav Singh <gp5in9h@gmail.com>
2021-03-11 04:49:47 +0530

    OOPs in Js done

--- javascript/Object Oriented/basic/basic.js
+++ javascript/Object Oriented/basic/basic.js
@@ -19,7 +19,92 @@ function colorMaker(r, g, b) {
     }
     return colors;
 }
-
+// objects using constructor functions
 const obj = colorMaker(12, 234, 19);
 console.log(obj.rgb());
 console.log(obj.hex());
+
+function Color(r,g,b){
+    this.r=r;
+    this.g=g;
+    this.b=b;
+    console.log(this);
+}
+
+Color.prototype.rgb = function () {
+    const {r,g,b} = this;
+    return `rgb(${r},${g},${b})`;
+}
+
+Color.prototype.rgba = function(a=1.0){
+    const {r,g,b} = this;
+    return `rgba(${r},${g},${b},${a})`;
+}
+
+const black = new Color(0,0,0); // here the new keyword triggers the constructor function
+
+// good way of using classes and objects
+class Color_2 {
+    constructor(r,g,b,name,customGreeting){
+        console.log("Inside Constructor");
+        console.log(r,g,b);
+        // adding properties to the class object
+        this.r=r;
+        this.g=g;
+        this.b=b;
+        this.name = name;
+        this.addAttributes(customGreeting);
+    }
+    greet(){
+        return `Hello from ${this.name}`;
+    }
+    innerRGB(){
+        return `${this.r},${this.g},${this.b}`;
+    }
+    rgb(){
+        return `rgb(${this.innerRGB()})`; // calling another function of the same class
+    }
+    addAttributes(customGreeting){
+        this.greeting = customGreeting; // adding another attribute to the class from inside the class
+    }
+}
+
+const c1 = new Color_2(255,67,89,"tomato","Hello There");
+
+class Pet{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    eat(){
+        console.log(`${this.name} is eating`);
+    }
+}
+
+class Cat extends Pet{
+    constructor(name,age,noOfLives=9){
+        super(name,age);
+        this.noOfLives = noOfLives;
+    }
+    meow(){
+        console.log("Meow");
+    }
+    // function overriding
+    eat(){
+        console.log(`${this.name} is eating fast!`);
+        super.eat();
+    }
+}
+class Dog extends Pet{
+    bark(){
+        console.log("woofff");
+    }
+}
+
+const Anakin = new Cat("Anakin",1);
+const kenobi = new Dog("kenobi",1);
+
+Anakin.eat();
+Anakin.meow();
+kenobi.eat();
+kenobi.bark();
