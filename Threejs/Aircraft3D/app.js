8f48d03c7d66438f84d6643708878ae0b88c7874
Gaurav Singh <gp5in9h@gmail.com>
2021-04-29 14:44:15 +0530

    updates to design

--- Threejs/Aircraft3D/app.js
+++ Threejs/Aircraft3D/app.js
@@ -11,16 +11,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 let obj;
+let flag = true;
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.minDistance = 100;
 controls.maxDistance = 1000;
 
 let loader = new GLTFLoader();
-loader.load('model/airplanemodelfinal.gltf', (gltf) => {
-    obj = gltf.scene;
-    scene.add(gltf.scene);
-});
+
+let modelloader = () => {
+    loader.load('model/airplanemodelfinal.gltf', (gltf) => {
+        obj = gltf.scene;
+        obj.name = "airplane";
+        scene.add(obj);
+    });
+}
+
+modelloader();
+
 
 scene.background = new THREE.Color(0x696969);
 
@@ -29,9 +37,28 @@ scene.add(light);
 camera.position.set(0, 50, 250);
 function animate(){
     requestAnimationFrame(animate);
-    obj.rotation.y += 0.005;
+    obj.rotation.y += 0.003;
     renderer.render(scene, camera);
 
     controls.update();
 }
+
+let selected;
+
+function toggleEntity(){
+    if (flag){
+        selected = scene.getObjectByName(obj.name);
+        scene.remove(selected);
+        animate();
+    }
+    else {
+        console.log(selected);
+    }
+    flag = !flag;
+}
+
+let btn = document.querySelector('#toggler');
+btn.addEventListener('click', toggleEntity);
+
+
 animate();
